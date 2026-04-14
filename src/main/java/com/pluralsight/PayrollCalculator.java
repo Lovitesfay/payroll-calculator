85ff56ec9d93b3893714afcec07a999b25897c5e
lovitesfay <lovitefay@gmail.com>
2026-04-13 21:31:38 -0500

    Added Methods

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,34 +1,41 @@
 package com.pluralsight;
 
+import javax.naming.Name;
 import java.util.Scanner;
 
 public class PayrollCalculator {
-
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-
-
-        Scanner payroll = new Scanner(System.in);
-
-        System.out.print("Enter Your Name:");
-
-        String Name = payroll.nextLine();
-
-        System.out.print("How many hours did you work? ");
-
-        double hoursWorked = payroll.nextDouble();
-
-        System.out.print("What is your hourly rate?");
-
-        double hourlyRate = payroll.nextDouble();
-
-        payroll.nextLine();
-
-        double result = hoursWorked * hourlyRate;
-
-        System.out.println("Lovi, You have made " + "$" + result + " for this week");
-
-
-
+ String name = payroll();
+ double hourworked = hours();
+ double houryrate = rate();
+ total(hourworked, houryrate);
 
     }
+    public static String payroll  (){
+
+        System.out.print("Enter Your Name: ");
+        String Name = input.nextLine();
+            return Name;
+    }
+    public static double hours()
+  {
+      System.out.print("How many hours did you work? ");
+      double hoursWorked = input.nextDouble();
+      return hoursWorked;
+}
+    public static double rate() {
+        System.out.print("What is your hourly rate? ");
+
+        double hourlyRate = input.nextDouble();
+
+        input.nextLine();
+        return hourlyRate;
+    }
+    public static void total(double hoursWorked, double hourlyRate){
+
+        double result = hoursWorked * hourlyRate;
+        System.out.println("Lovi, You have made " + "$" + result + " for this week");
+    }
+
 }
